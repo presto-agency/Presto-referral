dcf98f0ec2c905957ed0c676dcf463328fa93f76
Anton <kamarhin_anton@ukr.net>
2022-01-11 11:20:35 +0200

    feat: fix

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,27 +3,29 @@ window.addEventListener('DOMContentLoaded', function () {
 })
 
 const cursorAnimation = () => {
-    const cursor = document.querySelector('.block-1__arrow');
-    const block = window;
-    let links = document.querySelectorAll('a');
+    if (document.documentElement.clientWidth >= 1024) {
+        const cursor = document.querySelector('.block-1__arrow');
+        const block = window;
+        let links = document.querySelectorAll('a');
 
-    block.addEventListener('mouseover', function (e) {
-        cursor.style.display = 'block';
-        block.addEventListener('mousemove', cMove, false);
-        function cMove(e) {
-            cursor.style.left = e.pageX + 'px';
-            cursor.style.top = e.pageY + 'px';
-        }
-    }, false);
+        block.addEventListener('mouseover', function (e) {
+            cursor.style.display = 'block';
+            block.addEventListener('mousemove', cMove, false);
+            function cMove(e) {
+                cursor.style.left = e.pageX + 'px';
+                cursor.style.top = e.pageY + 'px';
+            }
+        }, false);
 
-    links.forEach(link => {
-        link.addEventListener('mouseover', function () {
-            cursor.classList.add('active');
-        })
-        link.addEventListener('mouseout', function () {
-            cursor.classList.remove('active');
-        })
-    });
+        links.forEach(link => {
+            link.addEventListener('mouseenter', function () {
+                cursor.classList.add('active');
+            })
+            link.addEventListener('mouseleave', function () {
+                cursor.classList.remove('active');
+            })
+        });
+    }
 }
 
 
